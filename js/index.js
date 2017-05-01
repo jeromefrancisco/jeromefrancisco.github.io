12f4075be8ef799526f10e6a32769db3ab42f4ee
Jerome Francisco <jeromefrancisco21@yahoo.com>
2017-04-30 21:13:12 -0700

    Added a couple projects, added welcome text and changed contact me section

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-$(function() {
-  $(".js-nav a").click(function(e) {
-    e.preventDefault();
-    $('body, html').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 750);
+$(document).ready(function(){
+  $(function() {
+    $(".js-nav a").click(function(e) {
+      e.preventDefault();
+      $('body, html').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 750);
+    });
   });
 });
