9038516f0f70419288b3c6b08244956b956cd4c8
Jerome Francisco <jeromefrancisco21@yahoo.com>
2017-04-30 21:15:43 -0700

    Revert "Added a couple projects, added welcome text and changed contact me section"
    This reverts commit 12f4075be8ef799526f10e6a32769db3ab42f4ee.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-  $(function() {
-    $(".js-nav a").click(function(e) {
-      e.preventDefault();
-      $('body, html').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-      }, 750);
-    });
+$(function() {
+  $(".js-nav a").click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
   });
 });
